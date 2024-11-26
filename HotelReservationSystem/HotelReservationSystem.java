8deeabec318a0ca5c359625f16b6023e85412d4e
Shailendra <shailendraverma1708@gmail.com>
2024-11-27 02:23:54 +0530

    init commit " Main Menu Added "

--- HotelReservationSystem/HotelReservationSystem.java
+++ HotelReservationSystem/HotelReservationSystem.java
@@ -3,6 +3,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 
 public class HotelReservationSystem {
@@ -24,7 +25,19 @@ public class HotelReservationSystem {
             con = DriverManager.getConnection(url,username,password);
 
             // Creating Main Menu
-
+while(true){
+    System.out.println();
+    System.out.println("HOTEL MANAGEMENT SYSTEM");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("1 Reserve your Room");
+    System.out.println("2 View Reservation");
+    System.out.println("3 Get your Room Number");
+    System.out.println("4 Update Reservation");
+    System.out.println("5 Delete Reservation");
+    System.out.println("0 Exit");
+    System.out.println("Choose an option: ");
+    int choice=sc.nextInt();
+}
         }
 
 
